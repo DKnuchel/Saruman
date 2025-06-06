2d7c4be62dfa85a31a30701edeac8919877a0fea
Yannic Lais <yannic.lais@students.fhnw.ch>
2025-06-06 14:36:34 +0200

    model

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ if not os.environ.get("OPENAI_API_KEY"):
 
 from langchain.chat_models import init_chat_model
 
-model = init_chat_model("gpt-4o-mini", model_provider="openai")
+model = init_chat_model("gpt-4.1-mini", model_provider="openai")
 
 from langchain.agents import initialize_agent, Tool
 from langchain.memory import ConversationBufferMemory
@@ -23,13 +23,13 @@ memory = ConversationBufferMemory(memory_key="chat_history")
 
 gandalf_prompt_tool = Tool(
   name="GandalfPromptTool",
-  fun=scp.main,
+  func=scp.main,
   description="Send prmpt to Gandalf and get response"
 )
 
 gandalf_password_tool = Tool(
   name="GandalfPasswordTool",
-  fun=scp.main_password, #TODO: password function
+  func=scp.main_password, #TODO: password function
   description="Use this to submit a guessed password to Gandalf."
 )
 
